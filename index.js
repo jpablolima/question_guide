02d739c4c46339a968c950cee61a3f33ba4c7806
jpablolima <jp.pablolima@gmail.com>
2021-01-28 12:41:45 -0300

    Configuration for using Bootstrap

--- index.js
+++ index.js
@@ -3,12 +3,21 @@ const app = express();
 
 const PORT = 3000;
 
-app.set('view engine', 'ejs')
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
 
 
 
 app.get('/', (req, res) => {
-    res.render('index')
+
+    const name = req.params.name;
+    const lang = req.params.lang;
+
+    res.render('index', {
+        name: name,
+        lang: lang,
+        age: 29
+    })
 })
 
 
